626bd75ada87b704735ea453e1fa1401131748e3
Claire1992 <cvc.hannon@gmail.com>
2015-04-13 16:23:46 +0100

    7th commit

--- AgDiagnose/src/com/example/agdiagnose/diseases/AnimalMenu.java
+++ AgDiagnose/src/com/example/agdiagnose/diseases/AnimalMenu.java
@@ -43,6 +43,8 @@ public class AnimalMenu extends Activity implements OnClickListener {
 		
 	}
 
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
